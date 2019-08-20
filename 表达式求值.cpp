52348a410ca5a79546b87f861cc97470ac10884b
lzh <luzehua@zhongbei.com>
2019-08-20 14:37:41 +0800

    表达式求值(后缀)

--- 表达式求值.cpp
+++ 表达式求值.cpp
@@ -1,245 +1,127 @@
-#include <iostream>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-#define OK 1
-#define ERROR 0
-#define TURE 1
-#define FLASE 0
-#define OVERFLOW -2
-
-#define STACK_INIT_SIZE 100
+#define STACK_INIT_SIZE 20
 #define STACKINCREMENT 10
+#define MAXBUFFER 10
 
-typedef char SElemType;
-typedef int Status;
+typedef float ElemType;
 
-typedef struct {
-	SElemType *base,*top;
-	int stacksize;
-}Sqstackchar;
-
-typedef struct {
-	double *base,*top;
-	int stacksize;
-}Sqstackdouble;
-
-Status InitStackchar(Sqstackchar &S);
-Status InitStackdouble(Sqstackdouble &S);
-SElemType GetTopchar(Sqstackchar &S,SElemType c);
-double GetTopdouble(Sqstackdouble &S,double c);
-Status Pushchar(Sqstackchar &S,SElemType &ch);
-Status Pushdouble(Sqstackdouble &S,double &ch);
-Status Popchar(Sqstackchar &S,SElemType &ch);
-Status Popdouble(Sqstackdouble &S,double &ch);
-Status StackEmpty(Sqstackchar S);//判断是否为空 
-Status Inn(SElemType c);//判断是否是运算符 
-double Operate(double a,SElemType theta,double b);//对数进行运算符
-SElemType Precede(SElemType ch1,SElemType ch2);//判断运算符的优先级 
-
-int main(int argc, char** argv) {
-	SElemType theta,ch,x,c;
-	double a=0,b;
-	double m;
-	Sqstackchar OPTR;
-	Sqstackdouble OPND;
-	
-	InitStackchar(OPTR);//初始化运算符栈 
-	InitStackdouble(OPND);//初始化运算数栈 
-	c='#';
-	Pushchar(OPTR,c);
-	printf("请输入一个带#结尾的表达式:");
-	ch=getchar();
-	
-	while(ch!='#' || GetTopchar(OPTR,c)!='#')
-	{
-		if(!Inn(ch))//不是运算符则进栈 
-		{
-			while(!Inn(ch))
-			{
-				a=a*10+(ch-48);
-				ch=getchar();	
-			}
-			Pushdouble(OPND,a);
-			printf("入OPND栈 %lf\n",a);
-		}
-		else
-		{
-			switch(Precede(GetTopchar(OPTR,c),ch))  //通过比较接收的字符来进行运算
-			{
-				case'<':              //栈顶元素优先权低 
-				Pushchar(OPTR,ch);
-				printf("入OPTR栈 %c\n",ch);
-				ch=getchar();
-				break;
-				
-				case'>':               //退栈并将运算结果入栈 
-				Popchar(OPTR,theta);
-				Popdouble(OPND,b);
-				Popdouble(OPND,a);
-				printf("入OPND栈前%lf %lf\n",b,a);
-				Pushdouble(OPND,m=Operate(a,theta,b));
-				printf("运算后 %lf\n",m);
-				break;
-				
-				case'=':                 //脱括号并接收下一个字符 
-				Popchar(OPTR,x);
-				ch=getchar();
-				break;
-			}
-			a=0;
-		}
-	}
-	printf("\n\n\n");
-    printf("运算结果= %lf",GetTopdouble(OPND,c));
-}
-
-Status InitStackchar(Sqstackchar &S)
+typedef struct
 {
-	S.base=(SElemType *)malloc(STACK_INIT_SIZE*sizeof(SElemType));
-	if(!S.base)
-		exit(OVERFLOW);
-	S.top=S.base;
-	S.stacksize=STACK_INIT_SIZE;
-	return OK;
-}
+    ElemType *base;
+    ElemType *top;
+    int stackSize;
+}sqStack;
 
-Status InitStackdouble(Sqstackdouble &S)
+
+
+void InitStack(sqStack *s)
 {
-	S.base=(double *)malloc(STACK_INIT_SIZE*sizeof(double));
-	if(!S.base)
-		exit(OVERFLOW);
-	S.top=S.base;
-	S.stacksize=STACK_INIT_SIZE;
-	return OK;
+    s->base=(ElemType *)malloc(STACK_INIT_SIZE*sizeof(ElemType));
+    if(!s->base)
+        exit(0);
+    s->top=s->base;
+    s->stackSize=STACK_INIT_SIZE;
 }
 
-SElemType GetTopchar(Sqstackchar &S,SElemType c)
+/*
+入栈操作
+*/
+void Push(sqStack *s,ElemType e)
 {
-	if(S.top==S.base)
-		return ERROR;
-	c=*(S.top-1);
-	return c;
-}
+    if(s->top-s->base==s->stackSize)
+    {
+        s->base=(ElemType *)realloc(s->base,(STACKINCREMENT+s->stackSize)*sizeof(ElemType));
+        if(!s->base)
+            exit(0);
 
-double GetTopdouble(Sqstackdouble &S,double c)
-{
-	if(S.top==S.base)
-		return ERROR;
-	c=*(S.top-1);
-	return c;
-}
-
-Status Pushchar(Sqstackchar &S,SElemType &ch)
-{
-	if(S.top-S.base>=S.stacksize)
-	{
-		S.base=(SElemType *)realloc(S.base,(S.stacksize+STACKINCREMENT)*sizeof(SElemType));
-		if(S.base==NULL)
-			exit(OVERFLOW);
-		S.top=S.base+S.stacksize;
-		S.stacksize+=STACKINCREMENT;
-	}
-	*(S.top++)=ch;
-	return OK;
-}
-
-Status Pushdouble(Sqstackdouble &S,double &ch)
-{
-	if(S.top-S.base>=S.stacksize)
-	{
-		S.base=(double *)realloc(S.base,(S.stacksize+STACKINCREMENT)*sizeof(double));
-		if(S.base==NULL)
-			exit(OVERFLOW);
-		S.top=S.base+S.stacksize;
-		S.stacksize+=STACKINCREMENT;
-	}
-	*(S.top++)=ch;
-	return OK;
-}
-
-Status Popchar(Sqstackchar &S,SElemType &ch)
-{
-	if(S.top==S.base)
-		return ERROR;
-	ch=*(--S.top);
-	return OK; 
-}
-
-Status Popdouble(Sqstackdouble &S,double &ch)
-{
-	if(S.top==S.base)
-		return ERROR;
-	ch=*(--S.top);
-	return OK; 
-}
-
-
-Status Inn(SElemType c)
-{
-	if(c=='+' || c=='-' ||c=='*' ||c=='/' || c=='#' || c=='(' ||c==')')
-		return OK;
-	else
-		return ERROR;
-}
-
-double Operate(double a,SElemType theta,double b)
-{
-	double c;
-	switch(theta)
-	{
-		case '+':c=a+b;
-		break;
-		case '-':c=a-b;
-		break;
-		case '*':c=a*b;
-		break;
-		case '/':c=a/b;
-		break;
-	}
-
-	return c;
-}
-
-SElemType  Precede(char a, char b){
-    int i,j;
-    char pre[][7]={         
-	/*运算符之间的优先级制作成一张表格*/
-	
-//		+ - * / ( ) # 
-//	
-//	+  	> > < < < > > 
-//	-	> > < < < > > 
-//	*	> > > > < > > 
-//	/	> > > > < > >
-//	(	< < < < < = 0 
-//	)	> > > > 0 > > 
-//	# 	< < < < < 0 = 
-        {'>','>','<','<','<','>','>'},
-        {'>','>','<','<','<','>','>'},
-        {'>','>','>','>','<','>','>'},
-        {'>','>','>','>','<','>','>'},
-        {'<','<','<','<','<','=','0'},
-        {'>','>','>','>','0','>','>'},
-        {'<','<','<','<','<','0','='}};
-    switch(a){
-        case '+': i=0; break;
-        case '-': i=1; break;
-        case '*': i=2; break;
-        case '/': i=3; break;
-        case '(': i=4; break;
-        case ')': i=5; break;
-        case '#': i=6; break;
+        s->top=s->base+s->stackSize;
+        s->stackSize=s->stackSize+STACKINCREMENT;
     }
-    switch(b){
-        case '+': j=0; break;
-        case '-': j=1; break;
-        case '*': j=2; break;
-        case '/': j=3; break;
-        case '(': j=4; break;
-        case ')': j=5; break;
-        case '#': j=6; break;
-    }
-    return pre[i][j];
+    *(s->top)=e;
+    s->top++;
 }
+
+void Pop(sqStack *s,ElemType *e)
+{
+    if(s->top==s->base)
+        return;
+    *e=*--(s->top);
+}
+
+int StackLen(sqStack *s)
+{
+    return (s->top-s->base);
+}
+
+int main()
+{
+    sqStack s;
+    char c;
+    float d,e;
+    char str[MAXBUFFER];
+    int i=0;
+
+    InitStack(&s);
+    printf("请输入逆波兰表达式输入待计算数据，数据与运算符之间用空格隔开，以$作为结束标志：\n");
+
+    scanf("%c",&c);
+    while(c!='$')
+    {
+        while(isdigit(c)||c=='.')
+        {
+            str[i++]=c;
+            str[i]='\0';
+            if(i>=10)
+            {
+                printf("error");
+                return -1;
+            }
+
+            scanf("%c",&c);
+            if(c==' ')
+            {
+                d=atof(str);
+                Push(&s,d);
+                i=0;
+                break;
+            }
+        }
+        switch(c){
+	        case '+':
+	            Pop(&s,&e);
+	            Pop(&s,&d);
+	            Push(&s,d+e);
+	            break;
+	        case '-':
+	            Pop(&s,&e);
+	            Pop(&s,&d);
+	            Push(&s,d-e);
+	            break;
+	        case '*':
+	            Pop(&s,&e);
+	            Pop(&s,&d);
+	            Push(&s,d*e);
+	            break;
+	        case '/':
+	            Pop(&s,&e);
+	            Pop(&s,&d);
+	            if(e!=0){
+	                Push(&s,d/e);
+	            }
+	            else
+	            {
+	                printf("error!");
+	                return -1;
+	            }
+	            break;
+        }
+        scanf("%c",&c);
+    }
+    Pop(&s,&d);
+    printf("the final result:%5.3f",d);
+    return 0;
+}
+
